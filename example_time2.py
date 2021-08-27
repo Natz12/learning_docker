bf67542a1b0f2b63324cba705725db1b10c94733
Natz12 <natyhv@gmail.com>
2021-08-27 19:45:48 -0400

    Fix dst directory

--- example_time2.py
+++ example_time2.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import time
 import pandas # We dont use this library in the code but we still import it to check the env is correct
 
-text_path = "data/time_running.txt"
+text_path = "time_running.txt"
 for i in range(5):
     now = datetime.now()
     print(now, " yay")
