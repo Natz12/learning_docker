8a2a267161861ff22dbd37bcfb7963657fd01598
Natz12 <natyhv@gmail.com>
2021-10-04 15:15:06 +0000

    Change dst directory

--- 3-Docker-GPU/example_time_gpu2.py
+++ 3-Docker-GPU/example_time_gpu2.py
@@ -2,8 +2,9 @@ from datetime import datetime
 import time
 import pandas # We dont use this library in the code but we still import it to check the env is correct
 import torch
-
-text_path = "time_running.txt"
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+text_path = os.path.join(dir_path,'data','time_running.txt')
 for i in range(5):
     now = datetime.now()
     print(now, " yay")
